a63668d8f7f993d9c9407cdc49071c740dde6d44
Emily-design-coder <emilyrobb2012@icloud.com>
2026-01-21 20:18:21 +1100

    initial commit

---
+++ scripthw.js
@@ -0,0 +1,8 @@
+function myfunc() {
+    document.getElementById("result").innerHTML =
+    document.getElementById("demo").firstChild.nodeValue;
+    document.getElementById("result1").innerHTML =
+    document.getElementById("demo").chileNodes[0].nodeValue;
+
+
+}
